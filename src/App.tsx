e9be5a31d9436cbf395a70881f698e9b97c4b25f
mouraCorazim <lucasmouracorazim1@gmail.com>
2021-03-04 14:03:49 -0300

    createElement can be more readable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ReactElement} from 'react'
+import React, {ReactElement, FunctionComponent, useState, createElement} from 'react'
 
 import {
   BrowserRouter as Router,
@@ -7,15 +7,13 @@ import {
 } from 'react-router-dom'
 
 import './App.css'
-import Footer from './components/Commons/Footer/Footer'
-import Header from './components/Commons/Header/Header'
 
 import Forbidden from './components/Forbidden/Forbidden'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Profile from './components/Profile/Profile'
 
-type RouteProp = {exact: boolean, path: string, component: React.FunctionComponent}
+type RouteProp = {exact: boolean, path: string, component: FunctionComponent}
 
 const routes: RouteProp[] = [
 
@@ -39,13 +37,11 @@ const routes: RouteProp[] = [
 function App(): ReactElement {
   return (
     <>
-      <Header />
       <Router>
         <Switch>
-          {routes.map((route: RouteProp) => <Route { ... route } />)}
+          {routes.map((route: RouteProp) => createElement(Route, { ... route }))}
         </Switch>
       </Router>
-      <Footer />
     </>
   );
 }
