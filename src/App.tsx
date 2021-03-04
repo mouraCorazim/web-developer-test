615de94e2fbb5904c517847d3abb8e183eda3017
mouraCorazim <lucasmouracorazim1@gmail.com>
2021-03-04 12:27:49 -0300

    Add initial app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import './App.css';
+import React, {ReactElement} from 'react'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import './App.css'
+import Footer from './components/Commons/Footer/Footer'
+import Header from './components/Commons/Header/Header'
+
+import Forbidden from './components/Forbidden/Forbidden'
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import Profile from './components/Profile/Profile'
+
+type RouteProp = {exact: boolean, path: string, component: React.FunctionComponent}
+
+const routes: RouteProp[] = [
+
+  { component: Login
+  , exact    : true
+  , path     : '/'         },
+
+  { component: Home
+  , exact    : true
+  , path     : '/home'     },
+
+  { component: Profile   
+  , exact    : true      
+  , path     : '/profile'  },
+
+  { component: Forbidden 
+  , exact    : true      
+  , path     : '/forbidden'},
+]
+
+function App(): ReactElement {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Router>
+        <Switch>
+          {routes.map((route: RouteProp) => <Route { ... route } />)}
+        </Switch>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
