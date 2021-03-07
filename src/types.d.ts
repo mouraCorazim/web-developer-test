1947f7e5291b5f7213a14186b77bed4226afe1c7
mouraCorazim <lucasmouracorazim1@gmail.com>
2021-03-07 12:18:34 -0300

    Add module react-transition-group

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,35 +1,63 @@
-import { ReactSVGElement, ReactElement, FunctionComponent, FunctionComponentElement, ReactChildren, Props, ReactNode } from "react"
+import { 
+    ReactSVGElement, 
+    ReactElement, 
+    FunctionComponent, 
+    Props, 
+    ReactNode } from "react"
 
 export = AppTypes
 export as namespace AppTypes
 
 declare namespace AppTypes{
-    type SubTextProps       = {text: string}
-    type PrincipalTextProps = {text: string}
-    type InfoCardTitleProps = {text: string}
-    type InfoCardTextProps  = {text: string}
-    type HeaderProps        = {children: ReactNode}
-    type CardsWrapperProps  = {children: ReactNode}
-    type InfoCardImageProps = {svg: ReactSVGElement}
-    type InfoCardProps      = {title: string, text: string, svg: ReactSVGElement}
-    type InputWrapperProps  = Props & {children: ReactNode}
-    type InputProps         = Props
-    type LabelProps         = Props
+    type SubTextProps              = {text: string}
+    type PrincipalTextProps        = {text: string}
+    type InfoCardTitleProps        = {text: string}
+    type InfoCardTextProps         = {text: string}
+    type HeaderProps               = {children: ReactNode}
+    type CardsWrapperProps         = {children: ReactNode}
+    type InfoCardImageProps        = Props & {svg: string}
+    type InputWrapperProps         = Props & {children: ReactNode}
+    type InputProps                = Props
+    type LabelProps                = Props
+    type ButtonProps               = Props & {value: string}
+    type RepositoryCardFooterProps = {language: string, updatedAt: string }
+
+    type InfoCardProps = {
+        title: string, 
+        text : string, 
+        svg  : string
+    }
     
-    type RouteProp          = {
+    type RouteProp = {
         component: FunctionComponent,
         exact    : boolean, 
         path     : string
     }
 
-    type InputWrapperChild  = {
+    type InputWrapperChild = {
         component: FunctionComponent,
         children : ReactNode
-        props    : InputProps | LabelProps
+        props    : InputProps | LabelProps | ButtonProps
     }
     
     type HeaderChild = { 
         component: FunctionComponent, 
         children : ReactNode
+    }
+
+    type RepositoryCardProps = {
+        title      : string, 
+        language   : string, 
+        updatedAt  : string,
+        description: string | null
+    }
+
+    type FollowingCardProps = {
+        avatar   : string,
+        user     : string,
+        login    : string, 
+        location : string | null,
+        bio      : string | null,
+        company  : string | null
     }
 }
